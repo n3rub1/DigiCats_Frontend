cef23bae3b84b91f0a7152fd3d596124c544a788
MiguelTDF <mcaruana@tdf.mt>
2022-11-17 13:49:05 +0100

    Project setup v2

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header>
+    <header className="bg-clr-primary">
       <img src="" alt="" />
       <p>DigiCat</p>
       <p>Register</p>
