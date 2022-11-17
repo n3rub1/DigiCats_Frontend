ed37369f1888a3898e0111d6653b84b00210d9dc
MiguelTDF <mcaruana@tdf.mt>
2022-11-17 16:12:36 +0100

    Header Finished

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Image from "next/image";
+import paw from "../public/paw.png";
 
 export default function Header() {
   return (
     <header className="bg-clr-primary">
-      <img src="" alt="" />
-      <p>DigiCat</p>
-      <div className="">
+      <div className="left_header">
+        <Image
+          className="header_icon"
+          alt="Image of a cats' paw"
+          src={paw}
+        ></Image>
+        <p>Digicats</p>
+      </div>
+      <div className="right_header">
         <p>Register</p>
         <p>Log In</p>
       </div>
