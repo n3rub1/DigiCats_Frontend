382b1934c90cff937ea765b756294ab19bcd5a6f
MiguelTDF <mcaruana@tdf.mt>
2022-11-18 16:05:14 +0100

    Pushed for pull

--- components/mainContent.tsx
+++ components/mainContent.tsx
@@ -8,27 +8,46 @@ export default function MainContent() {
         <h1>Sponser A Cat</h1>
         <p>View all</p>
       </div>
-      <Carousel cols={3} rows={1} gap={10} autoplay={5000} >
+      <div className="carousel_container">
+      <Carousel  cols={3} rows={1} gap={50}>
         <Carousel.Item>
-          <img width="100%" src="https://picsum.photos/800/600?random=1" />
+          <img
+            className="carousel_img"
+            src="https://picsum.photos/800/600?random=1"
+          />
         </Carousel.Item>
         <Carousel.Item>
-          <img width="100%" src="https://picsum.photos/800/600?random=2" />
+          <img
+            className="carousel_img"
+            src="https://picsum.photos/800/600?random=2"
+          />
         </Carousel.Item>
         <Carousel.Item>
-          <img width="100%" src="https://picsum.photos/800/600?random=3" />
+          <img
+            className="carousel_img"
+            src="https://picsum.photos/800/600?random=3"
+          />
         </Carousel.Item>
         <Carousel.Item>
-          <img width="100%" src="https://picsum.photos/800/600?random=3" />
+          <img
+            className="carousel_img"
+            src="https://picsum.photos/800/600?random=3"
+          />
         </Carousel.Item>
         <Carousel.Item>
-          <img width="100%" src="https://picsum.photos/800/600?random=3" />
+          <img
+            className="carousel_img"
+            src="https://picsum.photos/800/600?random=3"
+          />
         </Carousel.Item>
         <Carousel.Item>
-          <img width="100%" src="https://picsum.photos/800/600?random=3" />
+          <img
+            className="carousel_img"
+            src="https://picsum.photos/800/600?random=3"
+          />
         </Carousel.Item>
-        {/* ... */}
       </Carousel>
+      </div>
     </main>
   );
 }
